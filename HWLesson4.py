743096f3f259115fdf45e7bd5c3d275ec16f438d
Liudmila <lidiay@yandex.ru>
2023-03-04 17:25:02 +0300

    Домашка к уроку 4 добавила эталоны

--- HWLesson4.py
+++ HWLesson4.py
@@ -23,6 +23,27 @@
 
 # print('Числа из обоих наборов без повторений => ', set_rez)
 
+# Решение-эталон (на мой взгляд, мое проще!)
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
+# for i in k1:
+#     set_2.add(i)
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end=' ')
+
 # Задача 24: В фермерском хозяйстве в Карелии выращивают чернику.
 # Она растет на круглой грядке, причем кусты высажены только по окружности.
 # Таким образом, у каждого куста есть ровно два соседних. Всего на грядке растет N кустов.
@@ -49,3 +70,16 @@
 #     if temp > maxx:
 #         maxx = temp
 # print(maxx, 'ягод может собрать за один заход собирающий модуль')
+
+# Решение-эталон
+# n = int(input())
+# arr = list()
+# for i in range(n):
+#     x = int(input())
+#     arr.append(x)
+
+# arr_count = list()
+# for i in range(len(arr) - 1):
+#     arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+# arr_count.append(arr[-2] + arr[-1] + arr[0])
+# print(max(arr_count))
